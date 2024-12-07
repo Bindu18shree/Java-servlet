b1cd006f3d7dea33df01a3b8279d60a6670026b2
Bindu18shree <bindushreep.xworkz@gmail.com>
2024-12-07 19:08:34 +0530

    servlet request and response methods

---
+++ nuts/src/main/java/com/xworkz/nuts/servlet/FruitMango.java
@@ -0,0 +1,21 @@
+package com.xworkz.nuts.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/first", loadOnStartup = 1)
+public class FruitMango extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter printWriter = resp.getWriter();
+
+        printWriter.println("This is Mango Class......!");
+    }
+}
